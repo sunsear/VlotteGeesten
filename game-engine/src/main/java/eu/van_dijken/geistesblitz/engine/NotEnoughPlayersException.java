68b6345cb762d2092b2736183e5d7ae2350b895c
Wim <github@gwaihir.net>
2014-11-03 17:28:23 +0100

    Fix serialVersionUID warning

--- game-engine/src/main/java/eu/van_dijken/geistesblitz/engine/NotEnoughPlayersException.java
+++ game-engine/src/main/java/eu/van_dijken/geistesblitz/engine/NotEnoughPlayersException.java
@@ -1,4 +1,6 @@
 package eu.van_dijken.geistesblitz.engine;
 
 public class NotEnoughPlayersException extends RuntimeException {
+
+	private static final long serialVersionUID = -6869039731804912617L;
 }
