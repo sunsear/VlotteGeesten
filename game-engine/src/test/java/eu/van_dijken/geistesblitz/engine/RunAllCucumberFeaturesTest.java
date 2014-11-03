37361cab4ea917c2a727555872fee4a15914ce64
Martin van Dijken <martin@van-dijken.eu>
2014-11-03 11:28:30 +0100

    Renamed test so that it matches what it does

--- game-engine/src/test/java/eu/van_dijken/geistesblitz/engine/RunAllCucumberFeaturesTest.java
+++ game-engine/src/test/java/eu/van_dijken/geistesblitz/engine/RunAllCucumberFeaturesTest.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = "eu.van_dijken.geistesblitz.steps", monochrome = true,
         format = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt"})
-public class GameObjectsTest {
+public class RunAllCucumberFeaturesTest {
 
 }
